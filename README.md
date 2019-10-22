c93f236e27f3e9cd4f493b71842d6fbd1d987674
joe.george <joe.george@cabotsolutions.com>
2019-10-22 17:22:46 +0530

    signup api created and connected to cognito

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # cognito-nodejs
+
+This project is created to understand cognito authentication.
+
+# prerequisite
+
+Create a file cognito-config.js in the root of the project and add the following data.
+
+        exports.poolData = {    
+            UserPoolId : "", // Your user pool id here    
+            ClientId : "" // Your client id here
+            }; 
+        exports.pool_region = ''; //AWS region
+
+# Reference 
+https://medium.com/@prasadjay/amazon-cognito-user-pools-in-nodejs-as-fast-as-possible-22d586c5c8ec
+https://www.npmjs.com/package/amazon-cognito-identity-js
--- handler.js
+++ handler.js
@@ -4,6 +4,17 @@ const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const AWS = require('aws-sdk');
+const request = require('request');
+const jwkToPem = require('jwk-to-pem');
+const jwt = require('jsonwebtoken');
+global.fetch = require('node-fetch');
+
+const { poolData, pool_region } = require('./cognito-config');
+
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({
@@ -13,4 +24,23 @@ app.use(bodyParser.urlencoded({
 app.get('/api', function (req, res) {
   res.status(200).json({ "status": 1, "message": "API Working" });
 });
+
+app.post('/api/signup', function (req, res) {
+  var body = req.body;
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  var attributeList = [];
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:body['email']}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"custom:role",Value:body['role']}));
+
+  userPool.signUp(body['email'], body['password'], attributeList, null, function(err, result){
+      if (err) {
+          console.log(err);
+          return;
+      }
+      const cognitoUser = result.user;
+      console.log('user name is ' + cognitoUser.getUsername());
+      res.status(200).json({ "status": 1, "message": "user: "+cognitoUser.getUsername() +" successfully added" });
+  });
+});
+
 module.exports.handler = serverless(app);
